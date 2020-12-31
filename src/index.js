3c06da091c619addd5c9d744a963f7a8826af998
Ohm Srivastava <71198994+ohm-srivastava@users.noreply.github.com>
2020-12-31 15:13:43 -0700

    name

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ class Main extends React.Component {
         return (
             <div className="container">
                 <h1 className="title">Movie Search</h1>
+                <h3>Made by Ohm Srivastava using the TMDB API</h3>
                 <MovieSearch />
             </div>
         );
